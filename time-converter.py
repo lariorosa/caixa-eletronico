5c5a826c36ee6b5cc1f3d136d404584e653db701
Larissa Rosa <93224886+lariorosa@users.noreply.github.com>
2022-03-05 12:26:04 -0300

    Update time-converter.py

--- time-converter.py
+++ time-converter.py
@@ -1,3 +1,12 @@
+# Transformar um número inteiro em  horas, minutos e segundos.
+#Read an integer value, which is the duration in seconds of a certain event in a factory, and inform it expressed in hours:minutes:seconds.
+
+#Input
+#The input file contains an integer N.
+
+#Output
+#Print the read time in the input file (seconds) converted in hours:minutes:seconds like the following example.
+
 n = int(input("Escreva o valor que deseja converter: "))
 horas = int(n/3600)
 minutos = (n - (horas*3600))/60
